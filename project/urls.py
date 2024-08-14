fbbb5ffa0428f2702b1315fe6aa66e09ddf0f853
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-08-14 21:27:04 +0300

    update files

--- project/urls.py
+++ project/urls.py
@@ -13,13 +13,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    """
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from management.views import AppList
 from management.views import changeLanguage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
@@ -29,5 +30,6 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),  
     path('change-language',changeLanguage, name='set_language')
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
