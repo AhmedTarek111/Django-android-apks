fa6699f5c6e6025cf558dca5d7272821935eae7b
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-08-17 20:49:32 +0300

    update some changes

--- management/forms.py
+++ management/forms.py
@@ -5,3 +5,8 @@ class AppForm(forms.ModelForm):
     class Meta:
         model = App
         fields = ['name_en', 'name_fr', 'apk_file', 'first_screen_shot', 'second_screen_shot', 'video_recording', 'ui_hierarchy']
+
+class AppCreateUpdateForm(forms.ModelForm):
+    class Meta:
+        model = App
+        fields = ['name_en', 'name_fr', 'apk_file']
