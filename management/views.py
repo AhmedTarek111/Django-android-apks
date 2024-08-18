4a052a3d23765811a989d90ff58dd67d9b1a75f5
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-08-19 00:13:09 +0300

    finish the appuim script that excute the req

--- management/views.py
+++ management/views.py
@@ -5,7 +5,6 @@ from .models import App
 from .forms import AppForm,AppCreateUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import translation
-from .tasks import appium_test_app
 from django.conf import settings
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
@@ -14,6 +13,9 @@ import subprocess
 import time
 from django.core.files.base import ContentFile
 import base64
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class AppList(LoginRequiredMixin,ListView):
     model = App
@@ -85,16 +87,34 @@ def run_test(request, app_id):
         # connect to driver 
         driver = webdriver.Remote(command_executor='http://localhost:4723', options=options)
         print("Driver connected successfully")
-        
         # Start recording video
         driver.start_recording_screen()
         print("Started recording")
         time.sleep(5)
+        app.ui_hierarchy = driver.page_source
+        app.save()
         # taking first screenshot
         f_screenshot = driver.get_screenshot_as_png()
         print('Screen shot 1 Done')
+         # get the first button 
+        first_button = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@clickable="true"]'))
+        )
+        first_button.click()
+        print("first button clicked")
+        time.sleep(7)
+        if first_button :
+                app.screen_changed = True
+                app.save()
+                print('screen changed')
+            # taking second screenshot
 
+                s_screenshot = driver.get_screenshot_as_png()
+                print('Screen shot 2 Done')
+            
+            
         # Stop recording video
+        time.sleep(4)
         video_base64 = driver.stop_recording_screen()
         print("Stopped recording")
 
@@ -104,6 +124,11 @@ def run_test(request, app_id):
         # save f screenshot in db
         f_screenshot_file = ContentFile(f_screenshot , f_screenshot_name)
         app.first_screen_shot.save(f_screenshot_name,f_screenshot_file ,save=True)
+        
+        # save s screenshot in db
+        s_screenshot_file = ContentFile(s_screenshot , s_screenshot_name)
+        app.second_screen_shot.save(s_screenshot_name,s_screenshot_file ,save=True)
+        
     except Exception as e:
         print(f"An error occurred: {e}")
 
